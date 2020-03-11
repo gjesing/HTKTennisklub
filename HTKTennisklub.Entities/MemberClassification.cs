545a031eef3858a24b5aa7175ca841207a70eeca
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:33:34 +0100

    MemberClassification class created in Entities

---
+++ HTKTennisklub.Entities/MemberClassification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class MemberClassification
+    {
+        public int Id { get; set; }
+        public Member Member { get; set; }
+        public Classification Classification { get; set; }
+    }
+}
