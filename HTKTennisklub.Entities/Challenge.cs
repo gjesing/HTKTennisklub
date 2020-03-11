59cdd8a0f269ef5678a9f814b70e05b990a70f4a
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:37:24 +0100

    Challenge class created in Entities

---
+++ HTKTennisklub.Entities/Challenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class Challenge
+    {
+        public int Id { get; set; }
+        public Member Member1 { get; set; }
+        public Member Member2 { get; set; }
+        public Member Winner { get; set; }
+    }
+}
