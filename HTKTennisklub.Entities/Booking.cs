afae883f5cc1be03a349f048c5668e7479adca03
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:40:01 +0100

    Booking class created in Entities

---
+++ HTKTennisklub.Entities/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public Member Member1 { get; set; }
+        public Member Member2 { get; set; }
+    }
+}
