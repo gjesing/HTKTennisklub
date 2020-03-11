19f7368fad0482d5b278b0a34d12155cc85e5869
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:35:22 +0100

    Rank class created in Entities

---
+++ HTKTennisklub.Entities/Rank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class Rank
+    {
+        public int Id { get; set; }
+        public Member Member { get; set; }
+        public int Points { get; set; }
+        public int RankNumber { get; set; }
+    }
+}
