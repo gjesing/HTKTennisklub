b521754ef5cc17b61d3d0c6e7bdbf32bf1572989
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 12:55:44 +0100

    ClassificationRepository class created in DAL

--- HTKTennisklub.DAL/ClassificationRepository.cs
+++ HTKTennisklub.DAL/ClassificationRepository.cs
@@ -17,6 +17,13 @@ namespace HTKTennisklub.DAL
         public List<Classification> GetClassifications() => HandleData(ExecuteQuery("SELECT * FROM Classifications"));
 
         /// <summary>
+        /// Retrieves all Classifications with the specified MemberId from the database.
+        /// </summary>
+        /// <param name="memberId">The MemberId which is used to get only Classifications a specific Member belongs to</param>
+        /// <returns>A List of all Classifications with the specified MemberId</returns>
+        public List<Classification> GetClassifications(int memberId) => HandleData(ExecuteQuery($"SELECT Classifications.Id, Name FROM Classifications JOIN MemberClassifications ON MemberClassifications.ClassificationId = Classifications.Id WHERE MemberId = {memberId}"));
+
+        /// <summary>
         /// Helper method used to convert DataTable to list of Classifications.
         /// </summary>
         /// <param name="dataTable">The DataTable to be converted to list of Classifications</param>
