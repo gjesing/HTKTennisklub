9ce9e4ba6f0fff07f969c7267d777dd4733e2e9b
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:23:27 +0100

    Gender enum created in Entities

---
+++ HTKTennisklub.Entities/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public enum Gender
+    {
+        Female = 'f',
+        Male = 'm',
+        Unknown = 'u'
+    }
+}
