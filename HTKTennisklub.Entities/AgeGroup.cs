5a190780213b5c3830be3075c1bda833fed657a7
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:17:30 +0100

    AgeGroup class in Entities updated

--- HTKTennisklub.Entities/AgeGroup.cs
+++ HTKTennisklub.Entities/AgeGroup.cs
@@ -10,7 +10,7 @@ namespace HTKTennisklub.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int MinAge { get; set; }
-        public int MaxAge { get; set; }
+        public int? MinAge { get; set; }
+        public int? MaxAge { get; set; }
     }
 }
