6a34c54d43d29564846570f3e28699791d5b5f5b
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:20:17 +0100

    CourtClosing class created in Entities

---
+++ HTKTennisklub.Entities/CourtClosing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class CourtClosing
+    {
+        public int Id { get; set; }
+        public Court Court { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsPermanent { get; set; }
+    }
+}
