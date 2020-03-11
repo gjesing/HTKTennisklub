879aa7f067d17c6442c0c36148a6f5302b2ba138
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-03-11 11:30:47 +0100

    Member class created in Entities

---
+++ HTKTennisklub.Entities/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTKTennisklub.Entities
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public AgeGroup AgeGroup { get; set; }
+        public List<Classification> Classifications { get; set; }
+        public Level Level { get; set; }
+        public bool IsMember { get; set; }
+    }
+}
